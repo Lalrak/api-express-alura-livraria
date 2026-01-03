c3fd34bb046f87e3f76cdaf9e440f693c7358dc0
João Paulo <joaopaulo.silva2021@gmail.com>
2026-01-03 16:01:20 -0300

    feat: refatorar esquema de livro para melhor legibilidade e estrutura

--- src/models/livro.model.js
+++ src/models/livro.model.js
@@ -1,19 +1,33 @@
 import mongoose from "mongoose";
 
-const livroSchema = new mongoose.Schema({
-  id: { type: String },
-  titulo: {
-    type: String,
-    required: [true, "O título do livro é obrigatório."],
+const livroSchema = new mongoose.Schema(
+  {
+    id: { type: String },
+    titulo: {
+      type: String,
+      required: [true, "O título do livro é obrigatório."],
+    },
+    autor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores",
+      required: [true, "O(a) autor(a) é obrigatório(a)."],
+    },
+    editora: {
+      type: String,
+      required: [true, "A editora é obrigatória."],
+      enum: {
+        values: ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido.",
+      },
+    },
+    numeroPaginas: {
+      type: Number,
+      min: [10, "O número de páginas deve estar entre 10 e 5000"],
+      max: [5000, "O número de páginas deve estar entre 10 e 5000"],
+    },
   },
-  autor: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "autores",
-    required: [true, "O(a) autor(a) é obrigatório(a)."],
-  },
-  editora: { type: String, required: [true, "A editora é obrigatória."] },
-  numeroPaginas: { type: Number },
-});
+  { versionKey: false }
+);
 
 const livros = mongoose.model("livros", livroSchema);
 
